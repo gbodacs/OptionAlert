1af56271e46314359e196737a3b46827a07e1e74
Gabor Bodacs <gbodacs@gmail.com>
2022-09-24 19:58:40 +0200

    Alert button + new bagde

--- src/Ui/components/layout/NavBar.tsx
+++ src/Ui/components/layout/NavBar.tsx
@@ -1,5 +1,5 @@
 import { FunctionComponent, useEffect, useState } from "react";
-import { FaBars, FaFileAlt, FaList, FaPlus } from "react-icons/fa";
+import { FaBars, FaFileAlt, FaBell } from "react-icons/fa";
 import { FaRegSun } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import Spinner from "./Spinner";
@@ -10,7 +10,9 @@ type NavBarProps = {
 };
 
 const NavBar: FunctionComponent<NavBarProps> = ({ title }: NavBarProps) => {
-  const alerts = Global.getInstance().getUiConstManager().getNumberOfAlerts();
+  const alertNum: number = Global.getInstance().getUiConstManager().getNumberOfAlerts();
+  const isAlert: boolean = (alertNum > 0);
+
   return (
     <nav className="navbar mb-4 shadow-lg bg-neutral text-neutral-content">
       <div className="container mx-auto">
@@ -22,26 +24,30 @@ const NavBar: FunctionComponent<NavBarProps> = ({ title }: NavBarProps) => {
         </div>
         <div className="flex-1 px-1 mx-1 ">
           <div className="flex justify-end">
-            <div className="dropdown dropdown-end">
-              <label tabIndex={0} className="btn btn-ghost rounded-btn"><FaBars className="inline pr-3 text-3xl" />Menu</label>
-              <ul tabIndex={0} className="menu dropdown-content p-2 shadow bg-base-300 rounded-box w-52 mt-4">
+            { isAlert && ( <div className="indicator">
+                <span className="indicator-item badge badge-secondary">{alertNum}</span>
+                <Link to="/alertlist" className="btn btn-ghost rounded-btn bg-base-200 justify-start align-middle"><FaBell className="inline pr-3 text-3xl"/>Alert List</Link>
+                </div> )}
+
+            { !isAlert && (
+              <Link to="/alertlist" className="btn btn-ghost rounded-btn bg-base-200 justify-start align-middle"><FaBell className="inline pr-3 text-3xl"/>Alert List</Link>
+            )}
+            
+            <div className="dropdown dropdown-end px-4">
+              <label tabIndex={0} className="btn btn-ghost rounded-btn"><FaBars className="inline pr-3 text-3xl"/>Menu</label>
+              
+              <ul tabIndex={0} className="menu dropdown-content p-2 shadow bg-base-300 rounded-box w-52 m-4">
                 <li>
-                  <Link to="/" className="btn btn-ghost btn-sm rounded-btn justify-start">Add new</Link>
+                  <Link to="/" className="btn btn-ghost rounded-btn justify-start align-middle">Add new</Link>
                 </li>
                 <li>
-                  <div className="indicator">
-                    <span className="indicator-item badge badge-secondary">{alerts}</span> 
-                    <Link to="/alertlist" className="btn btn-ghost btn-sm rounded-btn justify-start">Alert List</Link>
-                  </div>
+                  <Link to="/watchlist" className="btn btn-ghost rounded-btn justify-start align-middle">Watchlist</Link>
                 </li>
                 <li>
-                  <Link to="/watchlist" className="btn btn-ghost btn-sm rounded-btn justify-start">Watchlist</Link>
+                  <Link to="/settings" className="btn btn-ghost rounded-btn justify-start align-middle">Settings</Link>
                 </li>
                 <li>
-                  <Link to="/settings" className="btn btn-ghost btn-sm rounded-btn justify-start">Settings</Link>
-                </li>
-                <li>
-                  <Link to="/about" className="btn btn-ghost btn-sm rounded-btn justify-start">About</Link>
+                  <Link to="/about" className="btn btn-ghost rounded-btn justify-start align-middle">About</Link>
                 </li>
               </ul>
             </div>
