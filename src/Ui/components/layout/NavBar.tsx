b3534b53bf7f56595ec633b8f7a411de7c3c5cdf
Gabor Bodacs <gbodacs@gmail.com>
2022-09-24 21:17:01 +0200

    NavBar final fix

--- src/Ui/components/layout/NavBar.tsx
+++ src/Ui/components/layout/NavBar.tsx
@@ -16,29 +16,17 @@ const NavBar: FunctionComponent<NavBarProps> = ({ title }: NavBarProps) => {
   return (
     <nav className="navbar mb-4 shadow-lg bg-neutral text-neutral-content">
       <div className="container mx-auto">
-        <div className="flex-none px-2 mx-2">
-          <Link to="/" className="text-lg font-bold align-middle">
-            <FaFileAlt className="inline pr-3 text-3xl" />
-            {title}
-          </Link>
-        </div>
-        <div className="flex-1 px-1 mx-1 ">
-          <div className="flex justify-end">
-            { isAlert && ( <div className="indicator">
-                <span className="indicator-item badge badge-secondary">{alertNum}</span>
-                <Link to="/alertlist" className="btn btn-ghost rounded-btn bg-base-200 justify-start align-middle"><FaBell className="inline pr-3 text-3xl"/>Alert List</Link>
-                </div> )}
-
-            { !isAlert && (
-              <Link to="/alertlist" className="btn btn-ghost rounded-btn bg-base-200 justify-start align-middle"><FaBell className="inline pr-3 text-3xl"/>Alert List</Link>
-            )}
-            
-            <div className="dropdown dropdown-end px-4">
-              <label tabIndex={0} className="btn btn-ghost rounded-btn"><FaBars className="inline pr-3 text-3xl"/>Menu</label>
+        <div className="flex-none">
+          
+        <div className="dropdown">
+              <label tabIndex={0} className="btn btn-ghost rounded-btn"><FaBars className="inline text-3xl pr-2"/>{title}</label>
               
               <ul tabIndex={0} className="menu dropdown-content p-2 shadow bg-base-300 rounded-box w-52 m-4">
                 <li>
                   <Link to="/" className="btn btn-ghost rounded-btn justify-start align-middle">Add new</Link>
+                </li>
+                <li>
+                  <Link to="/alertlist" className="btn btn-ghost rounded-btn justify-start align-middle">Alert List</Link>
                 </li>
                 <li>
                   <Link to="/watchlist" className="btn btn-ghost rounded-btn justify-start align-middle">Watchlist</Link>
@@ -51,6 +39,18 @@ const NavBar: FunctionComponent<NavBarProps> = ({ title }: NavBarProps) => {
                 </li>
               </ul>
             </div>
+        </div>
+
+        <div className="flex-1 px-1 mx-1 ">
+          <div className="flex justify-end">
+            { isAlert && ( <div className="indicator">
+                <span className="indicator-item badge badge-secondary">{alertNum}</span>
+                <Link to="/alertlist" className="btn btn-ghost rounded-btn bg-base-200 justify-start align-middle"><FaBell className="inline text-2xl"/></Link>
+                </div> )}
+
+            { !isAlert && (
+              <Link to="/alertlist" className="btn btn-ghost rounded-btn bg-base-200 justify-start align-middle"><FaBell className="inline text-2xl"/></Link>
+            )}
           </div>
         </div>
       </div>
